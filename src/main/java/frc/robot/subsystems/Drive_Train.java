2fd27f29be2d5f03feb9f390c196dd3cd09b5c10
Arpana Shrestha <shresthaa23@duchesne.org>
2022-01-21 17:41:52 -0600

    Co-authored-by: chesmith <chesmith@users.noreply.github.com> Co-authored-by: brookeblanton1 <brookeblanton1@users.noreply.github.com>

--- src/main/java/frc/robot/subsystems/Drive_Train.java
+++ src/main/java/frc/robot/subsystems/Drive_Train.java
@@ -10,13 +10,15 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.DrivetrainConstants;
+import frc.robot.Constants.JoystickConstants;
 
 public class Drive_Train extends SubsystemBase {
   
-  private final CANSparkMax _fLMotor = new CANSparkMax(1, MotorType.kBrushless);
-  private final CANSparkMax _fRMotor = new CANSparkMax(2, MotorType.kBrushless);
-  private final CANSparkMax _bLMotor = new CANSparkMax(3, MotorType.kBrushless);
-  private final CANSparkMax _bRMotor = new CANSparkMax(4, MotorType.kBrushless);
+  private final CANSparkMax _fLMotor = new CANSparkMax(DrivetrainConstants.frontLeftMotor, MotorType.kBrushless);
+  private final CANSparkMax _fRMotor = new CANSparkMax(DrivetrainConstants.frontRightMotor, MotorType.kBrushless);
+  private final CANSparkMax _bLMotor = new CANSparkMax(DrivetrainConstants.rearLeftMotor, MotorType.kBrushless);
+  private final CANSparkMax _bRMotor = new CANSparkMax(DrivetrainConstants.rearRightMotor, MotorType.kBrushless);
 
   private final DifferentialDrive _drive = new DifferentialDrive(_fLMotor, _fRMotor); 
 
@@ -31,15 +33,26 @@ public class Drive_Train extends SubsystemBase {
 
     _bLMotor.follow(_fLMotor);
     _bRMotor.follow(_fRMotor);
+
+    enableOpenLoopRampRate(true);
+  }
+
+  public void enableOpenLoopRampRate(boolean enable) {
+    double rampRate = (enable ? DrivetrainConstants.rampRate : 0.0);
+
+    _fLMotor.setOpenLoopRampRate(rampRate);
+    _fRMotor.setOpenLoopRampRate(rampRate);
+    _bLMotor.setOpenLoopRampRate(rampRate);
+    _bRMotor.setOpenLoopRampRate(rampRate);
   }
 
   public void teleopDrive(Joystick driveControl) {
-    double forward = driveControl.getRawAxis(1);
-    double turn = driveControl.getRawAxis(4); 
+    double forward = driveControl.getRawAxis(JoystickConstants.LEFT_STICK_Y);
+    double turn = driveControl.getRawAxis(JoystickConstants.RIGHT_STICK_X);
 
-    //_drive.curvatureDrive(forward, turn, Math.abs(forward) <= .1);
     _drive.arcadeDrive(forward, -turn);
   }
+  
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
